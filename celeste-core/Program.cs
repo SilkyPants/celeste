ebf551a771b3b4c3346ea7835a34868596745ccc
Dan Silk <dfsilk@gmail.com>
2020-04-05 20:57:34 +0930

    Initial commit
    PoC Messing around to see what is available.

---
+++ celeste-core/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using EliteAPI;
+using Somfic.Logging.Handlers;
+
+namespace celeste
+{
+    class MainClass
+    {
+        private static EliteDangerousAPI EliteAPI;
+
+        private static void Main(string[] args)
+        {
+            DirectoryInfo journalDirectory = new DirectoryInfo("../ed-data/");
+            EliteAPI = new EliteDangerousAPI(journalDirectory: journalDirectory, skipCatchUp: false);
+            EliteAPI.Logger.AddHandler(new ConsoleHandler());
+
+            EliteAPI.Start();
+
+            Thread.Sleep(-1);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
