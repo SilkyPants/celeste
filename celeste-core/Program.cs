12ff062c374cb777a5e73b2ec4f4e13345198db3
Dan Silk <dfsilk@gmail.com>
2020-04-09 22:12:17 +0930

    Add initial building blocks for serving app

--- celeste-core/Program.cs
+++ celeste-core/Program.cs
@@ -1,24 +1,51 @@
 ﻿using System;
 using System.IO;
-using System.Threading;
+using System.Runtime.InteropServices;
+
 using EliteAPI;
 using Somfic.Logging;
 using Somfic.Logging.Handlers;
 
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+
 namespace celeste
 {
+    /*
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+    */
+
+    
     class MainClass
     {
         private static EliteDangerousAPI EliteAPI;
 
         private static void Main(string[] args)
         {
-            //DirectoryInfo journalDirectory = new DirectoryInfo("../../ed-data/");
-            //EliteAPI = new EliteDangerousAPI(journalDirectory);
-            EliteAPI = new EliteDangerousAPI();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                EliteAPI = new EliteDangerousAPI();
+            }
+            else {
+                DirectoryInfo journalDirectory = new DirectoryInfo("../../ed-data/");
+                EliteAPI = new EliteDangerousAPI(journalDirectory);
+            }
 
             Logger.AddHandler(new LogFileHandler(Directory.GetCurrentDirectory(), "EliteAPI"));
-            Logger.AddHandler(new ConsoleHandler(), Severity.Info, Severity.Verbose, Severity.Debug, Severity.Warning, Severity.Error, Severity.Special, Severity.Success);
+            Logger.AddHandler(new ConsoleHandler(), Severity.Info, Severity.Verbose, Severity.Warning, Severity.Error, Severity.Success);
 
             EliteAPI.OnReset += (s, e) => {
                 EliteAPI.Events.CommanderEvent += HandleCommanderInfo;
@@ -27,14 +54,24 @@ namespace celeste
 
             EliteAPI.Start();
 
-            EliteAPI.Events.ShutdownEvent += HandleShutdown;
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                    .UseStartup<Startup>()
+                    .UseWebRoot("webapp");
+                }).Build();
 
+            host.RunAsync();
+
+            Console.WriteLine("Hit q to stop");
             while (EliteAPI.IsRunning) {
                 
                 var keyInfo = Console.ReadKey(intercept: true);
 
                 if (keyInfo.Key == ConsoleKey.Q) {
                     Console.WriteLine("Quitting...");
+                    host.StopAsync().Wait();
                     EliteAPI.Stop();
                 }
             }
@@ -73,5 +110,16 @@ namespace celeste
                 Console.WriteLine($"Unknown: {t.ToString()}");
             }
         }
+
+        //
+        //
+        //
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
---
+++ celeste-core/Startup.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Net.WebSockets;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace celeste
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseWebSockets();
+
+            app.Use(async (context, next) =>
+            {
+                if (context.WebSockets.IsWebSocketRequest)
+                {
+                    WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                    Console.WriteLine("WebSocket Connected");
+                }
+                else
+                {
+                    await next();
+                }
+            });
+        }
+    }
+}
